1c52595b39ac3c32a20efa836472a05efcc17ce0
james kisuule <kisuulejames1@gmail.com>
2019-02-05 14:30:37 +0300

    Add other endpoints to accommodate challenge 4

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 bcrypt = Bcrypt(app)
 Swagger(app)
 
-conn = psycopg2.connect(database = "ireporter",user="postgres", password="james",host="localhost",port=5432)
+# conn = psycopg2.connect(database = "ireporter",user="postgres", password="james",host="localhost",port=5432)
 if os.getenv('db') == 'heroku':
     conn = psycopg2.connect(database = "d2q6gssvm0bjnj",user="edsqjauzzqelnw", password="2f6a2ae7d655cdd784afc27a1afc49a2a555c458332537033bda700f25373e97",host="ec2-107-22-162-8.compute-1.amazonaws.com",port=5432)
 else: 
